4892d5eb279abbb46018dd7a1f8fe8f03169379c
mortalis13 <stangerroman@gmail.com>
2024-05-10 19:21:08 +0200

    fixed empty view error on calling mixin methods from event listeners

--- PrettyJsonListeners.py
+++ PrettyJsonListeners.py
@@ -10,6 +10,7 @@ class PrettyJsonLintListener(sublime_plugin.EventListener, PrettyJsonBaseCommand
     def on_post_save(self, view):
         if not s.get("validate_on_save", True):
             return
+        self.view = view
 
         as_json = s.get("as_json", ["JSON"])
         if any(syntax in view.settings().get("syntax") for syntax in as_json):
@@ -25,6 +26,7 @@ class PrettyJsonAutoPrettyOnSaveListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         if not s.get("pretty_on_save", False):
             return
+        self.view = view
 
         as_json = s.get("as_json", ["JSON"])
         if any(syntax in view.settings().get("syntax") for syntax in as_json):
